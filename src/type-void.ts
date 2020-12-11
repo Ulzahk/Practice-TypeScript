7f56cf34a8d6a51634747ccfebf3806f61fb74c6
Ulzahk <ulzahk@gmail.com>
2020-12-10 21:50:28 -0500

    [CREATED] files for void and never type

--- src/type-void.ts
+++ src/type-void.ts
@@ -7,3 +7,40 @@ function showInfo(user: any) {
 }
 
 showInfo({id: 1, username: 'ulzahk', firstName: 'Francisco'})
+
+// Tipo Inferido
+const showFormattedInfo = (user: any) => {
+  console.log('User Info', `
+    id: ${user.id},
+    username: ${user.username},
+    firstName: ${user.firstName}
+  `);
+};
+
+showFormattedInfo({id: 1, username: 'ulzahk', firstName: 'Francisco'});
+
+// Tipo Void, como tipo de dato de una variable
+let unusable: void;
+unusable = null;
+unusable = undefined;
+
+// Tipo: Never
+function handleError(code: number, message: string): never {
+  // Proceso de tu código
+  // Generar un mensaje
+  try {
+    throw new Error(`${message}. Code: ${code}`);
+  } catch (error) {
+    
+  }
+}
+handleError(404, 'Not Found');
+
+function sumNumber(limit: number): never {
+  let sum = 0;
+  while(true){
+    sum++
+  }
+}
+
+sumNumber(10); //Ciclo infinito;

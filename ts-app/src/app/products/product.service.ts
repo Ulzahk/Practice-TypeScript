526d786a6fde3180ec3a521b8bfd62f4cf470e4c
Ulzahk <ulzahk@gmail.com>
2022-09-26 07:14:24 -0500

    [UPDATE] Add initial structure CRUD ts example

--- ts-app/src/app/products/product.service.ts
+++ ts-app/src/app/products/product.service.ts
@@ -5,4 +5,16 @@ export const products: Product[] = [];
 export const addProducts = (data: Product) => {
   // data.id = 'abc';
   products.push(data);
+};
+
+export const getProduct = (id: string) => {
+  // code
+}
+
+export const updateProduct = (id: string, data: Product) => {
+  // code
+}
+
+export const deleteProduct = (id: string, data: Product) => {
+  // code
 }

6f261de0e27edaad33ac6b5d59ccdb84398c8865
Ulzahk <ulzahk@gmail.com>
2022-09-27 04:26:19 -0500

    [UPDATE] Add utility types omit and pick ts example

---
+++ ts-app/src/app/products/product.dto.ts
@@ -0,0 +1,7 @@
+import { Product } from './product.model';
+
+export interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
+  categoryId: string;
+};
+
+type pickExample = Pick<Product, 'color' | 'description'>
--- ts-app/src/app/products/product.service.ts
+++ ts-app/src/app/products/product.service.ts
@@ -1,10 +1,24 @@
 import { Product } from "./product.model";
+import { CreateProductDto } from "./product.dto";
+import { faker } from "@faker-js/faker";
 
 export const products: Product[] = [];
 
-export const addProducts = (data: Product) => {
-  // data.id = 'abc';
-  products.push(data);
+export const addProducts = (data: CreateProductDto): Product => {
+  const newProduct = {
+    ...data,
+    id: faker.datatype.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    category: {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    }
+  }
+  products.push(newProduct);
+  return newProduct;
 };
 
 export const getProduct = (id: string) => {

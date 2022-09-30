eb2dba54674fcdff58957b69f85db036376fb972
Ulzahk <ulzahk@gmail.com>
2022-09-30 06:06:51 -0500

    [UPDATE] Test commit with husky

---
+++ ts-oop/src/01-class.ts
@@ -0,0 +1,12 @@
+const date = new Date();
+date.getHours();
+date.getTime();
+date.toISOString();
+
+const date2 = new Date(1993, 1, 12); //0 January 11 dic
+date2.getHours();
+date2.getTime();
+date2.toISOString();
+
+console.log(date);
+console.log(date2)
